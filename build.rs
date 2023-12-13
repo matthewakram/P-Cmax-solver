748498774791cb2ed252e1c878d57de0847d3a08
Matthew Akram <mazfh85246@gmail.com>
2023-12-13 21:01:21 +0100

    updated workflow

--- build.rs
+++ build.rs
@@ -2,7 +2,7 @@ use std::process::Command;
 
 fn main() {
     if cfg!(target_os = "macos") {
-        let _ = Command::new("git")
+        let out = Command::new("git")
             .args([
                 "clone",
                 "https://github.com/arminbiere/kissat.git",
@@ -11,20 +11,36 @@ fn main() {
             .output()
             .expect("failed to execute process");
 
-        let _ = Command::new("./configure")
+        let str = std::str::from_utf8(&out.stdout).unwrap();
+        println!("{}", str);
+
+
+
+        let out = Command::new("./configure")
             .current_dir("./kissat_files")
             .output()
             .expect("failed to execute process");
 
-        let _ = Command::new("make")
+            let str = std::str::from_utf8(&out.stdout).unwrap();
+            println!("{}", str);
+
+        let out = Command::new("make")
             .current_dir("./kissat_files")
             .args(["test"])
             .output()
             .expect("failed to execute process");
 
+            let str = std::str::from_utf8(&out.stdout).unwrap();
+            println!("{}", str);
+
         let out = Command::new("mv")
             .args(["./kissat_files/build/kissat", "./kissat"])
             .output()
             .expect("failed to execute process");
+
+            let str = std::str::from_utf8(&out.stdout).unwrap();
+            println!("{}", str);
+
+            assert!(false);
     }
 }
