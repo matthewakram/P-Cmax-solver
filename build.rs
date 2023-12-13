aaf8aa3bfb1ec5c2184eb4a72b3f7abd96a394be
Matthew Akram <mazfh85246@gmail.com>
2023-12-13 20:20:48 +0100

    added support for automatically downloading kissat

---
+++ build.rs
@@ -0,0 +1,32 @@
+use std::process::Command;
+
+fn main() {
+    println!("cargo:rerun-if-changed=src/hello.c");
+
+    if cfg!(target_os = "macos") {
+        let _ = Command::new("git")
+            .args([
+                "clone",
+                "https://github.com/arminbiere/kissat.git",
+                "kissat_files",
+            ])
+            .output()
+            .expect("failed to execute process");
+
+        let _ = Command::new("./configure")
+            .current_dir("./kissat_files")
+            .output()
+            .expect("failed to execute process");
+
+        let _ = Command::new("make")
+            .current_dir("./kissat_files")
+            .args(["test"])
+            .output()
+            .expect("failed to execute process");
+
+        let _ = Command::new("mv")
+            .args(["./kissat_files/build/kissat", "./kissat"])
+            .output()
+            .expect("failed to execute process");
+    }
+}
