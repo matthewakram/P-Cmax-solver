d3c8aea111221a355daafbbf221ecc0e00153c31
Matthew Akram <mazfh85246@gmail.com>
2023-12-13 21:16:26 +0100

    updated workflow

--- build.rs
+++ build.rs
@@ -2,8 +2,6 @@ use std::process::Command;
 
 fn main() {
     if cfg!(target_os = "macos") {
-        println!("cargo:rerun-if-changed=\"./build.rs\"");
-        println!("cargo:rerun-if-changed=\"./kissat\"");
         let out = Command::new("git")
             .args([
                 "clone",
